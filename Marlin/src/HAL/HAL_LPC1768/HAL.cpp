645fe6df10a58799089956738f15307369fe0c3a
Thomas Moore <tcm0116@gmail.com>
2018-01-05 18:07:01 +0000

    Simplify median filter

--- Marlin/src/HAL/HAL_LPC1768/HAL.cpp
+++ Marlin/src/HAL/HAL_LPC1768/HAL.cpp
@@ -136,87 +136,25 @@ bool HAL_adc_finished(void) {
 #define ADC_USE_LOWPASS_FILTER     // filter out high frequency noise
 #define ADC_LOWPASS_K_VALUE 4      // how much to smooth out noise (1:8)
 
-// Sourced from https://embeddedgurus.com/stack-overflow/tag/median-filter/
 struct MedianFilter {
-  #define STOPPER 0                // Smaller than any datum
-  struct Pair {
-    Pair   *point;                 // Pointers forming list linked in sorted order
-    uint16_t  value;               // Values to sort
-  };
+  uint16_t values[ADC_MEDIAN_FILTER_SIZE];
+  uint8_t next_val;
+  
+  MedianFilter() {
+    next_val = 0;
+    memset(values, 0, sizeof(values));
+  }
 
-  Pair buffer[ADC_MEDIAN_FILTER_SIZE] = {}; // Buffer of nwidth pairs
-  Pair *datpoint = buffer;                  // Pointer into circular buffer of data
-  Pair small = {NULL, STOPPER};             // Chain stopper
-  Pair big = {&small, 0};                   // Pointer to head (largest) of linked list.
-
-  uint16_t update(uint16_t datum) {
-    Pair *successor;                        // Pointer to successor of replaced data item
-    Pair *scan;                             // Pointer used to scan down the sorted list
-    Pair *scanold;                          // Previous value of scan
-    Pair *median;                           // Pointer to median
-    uint16_t i;
-
-    if (datum == STOPPER) {
-      datum = STOPPER + 1;                  // No stoppers allowed.
-    }
-
-    if ( (++datpoint - buffer) >= ADC_MEDIAN_FILTER_SIZE) {
-      datpoint = buffer;                    // Increment and wrap data in pointer.
-    }
-
-    datpoint->value = datum;                // Copy in new datum
-    successor = datpoint->point;            // Save pointer to old value's successor
-    median = &big;                          // Median initially to first in chain
-    scanold = NULL;                         // Scanold initially null.
-    scan = &big;                            // Points to pointer to first (largest) datum in chain
-
-    // Handle chain-out of first item in chain as special case
-    if (scan->point == datpoint) {
-      scan->point = successor;
-    }
-    scanold = scan;                         // Save this pointer and
-    scan = scan->point ;                    // step down chain
-
-    // Loop through the chain, normal loop exit via break.
-    for (i = 0 ; i < ADC_MEDIAN_FILTER_SIZE; ++i) {
-      // Handle odd-numbered item in chain
-      if (scan->point == datpoint) {
-        scan->point = successor;            // Chain out the old datum
-      }
-
-      if (scan->value < datum) {            // If datum is larger than scanned value
-        datpoint->point = scanold->point;   // Chain it in here
-        scanold->point = datpoint;          // Mark it chained in
-        datum = STOPPER;
-      }
-
-      // Step median pointer down chain after doing odd-numbered element
-      median = median->point;               // Step median pointer
-      if (scan == &small) {
-        break;                              // Break at end of chain
-      }
-      scanold = scan;                       // Save this pointer and
-      scan = scan->point;                   // step down chain
-
-      // Handle even-numbered item in chain.
-      if (scan->point == datpoint) {
-        scan->point = successor;
-      }
-
-      if (scan->value < datum) {
-        datpoint->point = scanold->point;
-        scanold->point = datpoint;
-        datum = STOPPER;
-      }
-
-      if (scan == &small) {
-        break;
-      }
-
-      scanold = scan;
-      scan = scan->point;
-    }
-    return median->value;
+  uint16_t update(uint16_t value) {
+    uint16_t sorted_values[ADC_MEDIAN_FILTER_SIZE];
+    
+    values[next_val++] = value;
+    next_val = next_val % num_values;
+    
+    memcpy(sorted_values, values, sizeof(sorted_values));
+    std::sort(sorted_values, sorted_values + ADC_MEDIAN_FILTER_SIZE);
+    
+    return sorted_Values[(ADC_MEDIAN_FILTER_SIZE / 2) + 1];
   }
 };
 
